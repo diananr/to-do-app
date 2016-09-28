12a5df0e75d0cee6fd7b9045b0477965fff20f82
diananr <navarroruiz.diana>
2016-09-28 17:43:55 -0500

    agregando ejercicio

---
+++ js/app.js
@@ -0,0 +1,50 @@
+window.addEventListener("load", function(){
+	var areaTexto = document.getElementById("textArea");
+	var btnAgregar = document.getElementById("btn");
+	var cajaTareas = document.getElementById("listaTarea");
+
+	areaTexto.addEventListener("keyup", function(){
+		if(areaTexto.value.trim().length == 0 || areaTexto.value.trim() == "" ){
+			btnAgregar.disabled = true;
+		}
+		else
+			btnAgregar.disabled = false;
+	});
+
+	btnAgregar.addEventListener("click", function(){
+		agregarTarea();
+		areaTexto.value = "";
+		btnAgregar.disabled = true;
+	});
+
+	function agregarTarea(){
+		var tarea = document.createElement("div");
+		tarea.innerText = areaTexto.value.trim();
+		tarea.classList.add("cadaTarea");
+		cajaTareas.insertBefore(tarea, cajaTareas.childNodes[0]);
+
+		var check = document.createElement("input");
+		check.setAttribute("type", "checkbox");
+		check.classList.add("checki");
+		tarea.insertBefore(check,tarea.childNodes[0]);
+
+		check.addEventListener("click", function(){
+			if(this.checked){
+				this.parentElement.classList.add("tachado");
+			}
+			else
+				this.parentElement.classList.remove("tachado");
+		});
+
+		var cuadrado = document.createElement("button");
+		cuadrado.type = "button";
+		cuadrado.innerText = "X";
+		cuadrado.classList.add("tacho");
+		tarea.insertBefore(cuadrado,tarea.childNodes[3]);
+		// tarea.appendChild(cuadrado);
+
+		cuadrado.addEventListener("click", function(){
+			this.parentElement.style.display = "none";
+		});
+	}
+});
